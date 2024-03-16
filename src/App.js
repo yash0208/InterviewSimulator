9edb82418da2ddb240c45f3eeee25c8ec3a6deaa
priyankavaghela <priyanka4footprints@gmail.com>
2024-03-16 00:50:42 -0400

    Audio and Video Interview Pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import logo from './logo.svg';
-import {Routes, Route} from 'react-router-dom'
-import { BrowserRouter } from 'react-router-dom';
+import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import './App.css';
-import {Authentication} from "./Authentication/Authentication";
-import './fonts.css';
+import "./App.css";
+import { Authentication } from "./Authentication/Authentication";
+import "./fonts.css";
 import InterviewerScreen from "./InterviewerScreen";
 import QuizPage from "./QuizPage";
 import CandidateScreen from "./CandidateScreen";
-
+import AudioInterview from "./AudioInterview";
+import VideoInterview from "./VideoInterview";
 
 function App() {
   return (
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Authentication />}/>
-            <Route path='interviewer' element={<InterviewerScreen/>}/>
-              <Route path='candidate' element={<CandidateScreen/>}/>
-              <Route path='interviewer/create_quiz' element={<QuizPage/>}/>
-              <Route path='interviewer/create_quiz/interviewer' element={<InterviewerScreen/>}/>
-          </Routes>
-        </BrowserRouter>
-
-      </>
-
-
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Authentication />} />
+          <Route path="interviewer" element={<InterviewerScreen />} />
+          <Route path="candidate" element={<CandidateScreen />} />
+          <Route path="interviewer/create_quiz" element={<QuizPage />} />
+          <Route
+            path="interviewer/create_quiz/interviewer"
+            element={<InterviewerScreen />}
+          />
+          <Route
+            path="/candidate/audio-interview"
+            element={<AudioInterview />}
+          />
+          <Route
+            path="/candidate/video-interview"
+            element={<VideoInterview />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
