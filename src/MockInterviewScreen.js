d49fcb67cb2b7ed6ba565d747f56d3c0ad48e1ec
Nisarg Shah <nisargs2001@gmail.com>
2024-03-17 19:49:16 -0400

    video audio backend integration

--- src/MockInterviewScreen.js
+++ src/MockInterviewScreen.js
@@ -3,6 +3,33 @@ import styled, { createGlobalStyle } from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { child, get, getDatabase, ref } from "firebase/database";
 import * as Components from "./AudioComponents.js";
+import axios from "axios";
+import { set, push } from "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import {
+  getStorage,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { AudioRecorder } from "react-audio-voice-recorder";
+import { ref as sRef } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIVEam2B1Ws23SW43S3ebkc5lA7aGoVzo",
+  authDomain: "interviewsimulator-252d6.firebaseapp.com",
+  projectId: "interviewsimulator-252d6",
+  storageBucket: "interviewsimulator-252d6.appspot.com",
+  messagingSenderId: "106778466397",
+  appId: "1:106778466397:web:1340f64fdf73be004095b4",
+  measurementId: "G-92QYXMMFTH",
+};
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -17,102 +44,208 @@ const StyledTextInput = styled.textarea`
   height: 200px; /* Adjust height to fit 10 lines */
   padding: 10px;
   border: none;
-  background-color: #F3F3F3;
+  background-color: #f3f3f3;
   border-radius: 5px;
-  box-shadow: 7px 4px 37px -15px rgba(0,0,0,0.89);
+  box-shadow: 7px 4px 37px -15px rgba(0, 0, 0, 0.89);
   outline: none;
   resize: vertical; /* Allow vertical resizing */
 `;
 
 function VideoInterview() {
-    const [questionIndex, setQuestionIndex] = useState(0);
-    const [questions, setQuestions] = useState([]);
-    const [question, setQuestion] = useState(null); // Set initial state to null
-    const [answer, setAnswer] = useState("");
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [questions, setQuestions] = useState([]);
+  const [question, setQuestion] = useState(null); // Set initial state to null
+  const [answer, setAnswer] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+  const [apiLink, setApiLink] = useState("http://127.0.0.1:8080/video_feed");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        alert(
-            "This is a mock interview consisting 3 sections Video, audio, and text answers. At the end, you will be able to check the complete report for your answers."
-        );
-        const fetchData = async () => {
-            const dbRef = ref(getDatabase());
-            const snapshot = await get(child(dbRef, 'interviews/MockInterview/questions'));
-            if (snapshot.exists()) {
-                const interviewsData = [];
-                snapshot.forEach((childSnapshot) => {
-                    interviewsData.push({ id: childSnapshot.key, ...childSnapshot.val() });
-                });
-                setQuestions(interviewsData);
-                setQuestion(interviewsData[0]); // Set the initial question after fetching data
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    const goBack = () => navigate("/candidate");
-
-    const startRecording = () => {
-        // Logic to start recording
+  useEffect(() => {
+    alert(
+      "This is a mock interview consisting 3 sections Video, audio, and text answers. At the end, you will be able to check the complete report for your answers."
+    );
+    const fetchData = async () => {
+      const dbRef = ref(getDatabase());
+      const snapshot = await get(
+        child(dbRef, "interviews/MockInterview/questions")
+      );
+      if (snapshot.exists()) {
+        const interviewsData = [];
+        snapshot.forEach((childSnapshot) => {
+          interviewsData.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val(),
+          });
+        });
+        setQuestions(interviewsData);
+        setQuestion(interviewsData[0]); // Set the initial question after fetching data
+      }
     };
 
-    const submitRecording = () => {
+    fetchData();
+  }, []);
+
+  const startRecording = () => {
+    if (question.section === "video") {
+      setIsRecording(true);
+    } else if (question.section === "audio") {
+    }
+  };
+
+  const uploadAudioToFirebase = async (blob) => {
+    const storageRef = sRef(storage, `audio/${Date.now()}.wav`);
+    const uploadTask = await uploadBytesResumable(storageRef, blob);
+
+    // Get download URL
+    const downloadURL = await getDownloadURL(storageRef);
+
+    axios
+      .post("http://127.0.0.1:8080/audio_analysis", { link: downloadURL })
+      .then((response) => {
+        // Check if response data contains emotion
+        if (response.data) {
+          console.log("Predicted emotion:", response.data);
+          const user = auth.currentUser;
+          const db = getDatabase();
+          push(
+            ref(db, "completed-interviews/mockInterviews/" + user.uid + "mock"),
+            {
+              creator: user.uid,
+              candidate: user.uid,
+              questionId: questionIndex,
+              question: questions[questionIndex],
+              response: response.data,
+              link: downloadURL,
+            }
+          );
+        } else {
+          console.error("No result received");
+        }
         setQuestionIndex((prevIndex) => prevIndex + 1);
         if (questionIndex < questions.length - 1) {
-            setQuestion(questions[questionIndex + 1]);
+          setQuestion(questions[questionIndex + 1]);
         } else {
-            alert("All questions answered. Submitting recording...");
+          alert("All questions answered. Submitting recording...");
         }
-    };
+      })
+      .catch((error) => {
+        console.error("Error stopping recording:", error);
+      });
+  };
 
-    if (!question) {
-        return null; // Render nothing until questions are fetched
+  const addAudioElement = (blob) => {
+    // Upload audio to Firebase Storage
+    uploadAudioToFirebase(blob);
+  };
+
+  const submitRecording = () => {
+    setIsRecording(false);
+    axios
+      .get("http://127.0.0.1:8080/close_camera")
+      .then((response) => {
+        // Check if response data contains emotion
+        if (response.data) {
+          console.log("Predicted emotion:", response.data);
+          const user = auth.currentUser;
+          const db = getDatabase();
+          push(
+            ref(db, "completed-interviews/mockInterviews/" + user.uid + "mock"),
+            {
+              creator: user.uid,
+              candidate: user.uid,
+              questionId: questionIndex,
+              question: questions[questionIndex],
+              response: response.data,
+            }
+          );
+        } else {
+          console.error("No emotion data received");
+        }
+      })
+      .catch((error) => {
+        console.error("Error stopping recording:", error);
+      });
+    setApiLink("http://127.0.0.1:8080/video_feed");
+
+    setQuestionIndex((prevIndex) => prevIndex + 1);
+    if (questionIndex < questions.length - 1) {
+      setQuestion(questions[questionIndex + 1]);
+    } else {
+      alert("All questions answered. Submitting recording...");
     }
+  };
 
-    let bannerText = '';
-    let buttonText = '';
-    if (question.section === 'video') {
-        bannerText = 'Video Question';
-        buttonText = 'Start Recording (15s)';
-    } else if (question.section === 'audio') {
-        bannerText = 'Audio Question';
-        buttonText = 'Start Recording (1min)';
-    } else if (question.section === 'text') {
-        bannerText = 'Text Question';
-        buttonText = 'Start Typing';
-    }
+  if (!question) {
+    return null; // Render nothing until questions are fetched
+  }
 
-    return (
-        <>
-            <GlobalStyle />
-            <Components.ContainerWrapper>
-                <Components.Banner>{bannerText}</Components.Banner>
-                <Components.BlockQuote>
-                    <Components.Paragraph class="quotation">
-                        {question.question}
-                    </Components.Paragraph>
-                </Components.BlockQuote>
-                {question.section === 'text' && (
-                    <StyledTextInput
-                        value={answer}
-                        onChange={(e) => setAnswer(e.target.value)}
-                        placeholder="Type your answer here..."
-                    />
-                )}
-                <Components.Button onClick={startRecording}>
-                    {buttonText}
-                </Components.Button>
-                <Components.Button onClick={submitRecording}>
-                    Submit Recording
-                </Components.Button>
-                <Components.Message>{question.message}</Components.Message>
-                <Components.Anchor href="#">How does it work?</Components.Anchor>
-                <Components.BackButton onClick={goBack}>Back</Components.BackButton>
-            </Components.ContainerWrapper>
-        </>
-    );
+  let bannerText = "";
+  let buttonText = "";
+  if (question.section === "video") {
+    bannerText = "Video Question";
+    buttonText = "Start Recording";
+  } else if (question.section === "audio") {
+    bannerText = "Audio Question";
+    buttonText = "Start Recording (1min)";
+  } else if (question.section === "text") {
+    bannerText = "Text Question";
+    buttonText = "Start Typing";
+  }
+
+  return (
+    <>
+      <GlobalStyle />
+      <Components.ContainerWrapper>
+        <Components.Banner>{bannerText}</Components.Banner>
+        <Components.BlockQuote>
+          <Components.Paragraph class="quotation">
+            {question.question}
+          </Components.Paragraph>
+        </Components.BlockQuote>
+        {question.section === "text" && (
+          <StyledTextInput
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+            placeholder="Type your answer here..."
+          />
+        )}
+
+        {question.section === "audio" && (
+          <div>
+            <AudioRecorder
+              onRecordingComplete={addAudioElement}
+              audioTrackConstraints={{
+                noiseSuppression: true,
+                echoCancellation: true,
+              }}
+              downloadOnSavePress={false}
+              downloadFileExtension="wav"
+            />
+          </div>
+        )}
+
+        {question.section === "video" && isRecording && (
+          <img
+            key={apiLink}
+            src={apiLink}
+            alt="Video"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        )}
+
+        <Components.Button onClick={startRecording}>
+          {buttonText}
+        </Components.Button>
+        {isRecording && (
+          <Components.Button onClick={submitRecording}>
+            Stop Recording
+          </Components.Button>
+        )}
+        {/* <Components.Message>{question.message}</Components.Message> */}
+      </Components.ContainerWrapper>
+    </>
+  );
 }
 
 export default VideoInterview;
