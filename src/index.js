69564cec0f14623d79c981ff18c61a00349d8865
dhruv patel <dhruvppatel2201@gmail.com>
2024-03-20 01:49:03 -0400

    about css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ReportPage from './Report';
+import ThankYouPage from './ThankYou';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
